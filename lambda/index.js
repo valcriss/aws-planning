248a3953d1454a5c5bfd693942ca7a1c531184c1
Daniel Silvestre <dsilvestre@partouche.com>
2020-08-31 17:10:08 +0200

    Working Progress

--- lambda/index.js
+++ lambda/index.js
@@ -1,18 +1,24 @@
 /*******************************************************************************/
-
-var configurationFileParams = {
-    Bucket: "app-deploy.ptech.fr",
-    Key: "configuration.json",
-};
-
-// Time zone
-process.env.TZ = 'Europe/Paris'
-
+// CONFIGURATION
+/*******************************************************************************/
+var configuration = {
+    AWS_S3Bucket:"app-deploy.ptech.fr",
+    AWS_S3ConfigurationFile :"configuration.json",
+    AWS_Region :"eu-west-1",
+    DryRun:true,
+    TimeZone : "Europe/Paris",
+}
 
 /*******************************************************************************/
 
+var configurationFileParams = {
+    Bucket: configuration.AWS_S3Bucket,
+    Key: configuration.AWS_S3ConfigurationFile,
+};
+var dryRun = configuration.DryRun;
+process.env.TZ = configuration.TimeZone
 var aws = require('aws-sdk');
-aws.config.region = 'eu-west-1';
+aws.config.region = configuration.AWS_Region;
 var ec2 = new aws.EC2();
 var rds = new aws.RDS();
 var s3 = new aws.S3();
@@ -45,6 +51,8 @@ exports.handler = async (event) => {
     } else {
         response = checkInstances();
     }
+
+    console.log(response);
 
     return response;
 };
@@ -98,33 +106,12 @@ function listExisting(scope) {
 }
 
 function checkInstances() {
-    return new Promise(function (resolve) {
-        var orders = [];
-        loadConfigurationFromFile().then(function (values) {
-            return describeEc2instance(values);
-        }).then(function (values) {
-            for (let i = 0; i < values.length; i++) {
-                let desiredState = currentState(values[i]);
-                if (values[i].Status === "STOPPED" && desiredState === "RUNNING") {
-                    orders.push({instanceId: values[i].instanceId, type: "ec2", order: "start"});
-                } else if (values[i].Status === "RUNNING" && desiredState === "STOPPED") {
-                    orders.push({instanceId: values[i].instanceId, type: "ec2", order: "stop"});
-                }
-            }
-            return loadConfigurationFromFile().then(function (value) {
-                return describeRdsinstance(values);
-            });
-        }).then(function (values) {
-            for (let i = 0; i < values.length; i++) {
-                let desiredState = currentState(values[i]);
-                if (values[i].Status === "STOPPED" && desiredState === "RUNNING") {
-                    orders.push({instanceId: values[i].instanceId, type: "rds", order: "start"});
-                } else if (values[i].Status === "AVAILABLE" && desiredState === "STOPPED") {
-                    orders.push({instanceId: values[i].instanceId, type: "rds", order: "stop"});
-                }
-            }
-            resolve(responseSuccess(orders));
-        });
+    var orders = [];
+
+    return checkEc2Instances(orders).then(function (orders) {
+        return checkRdsInstances(orders);
+    }).then(function (orders) {
+        return responseSuccess(orders);
     });
 }
 
@@ -132,7 +119,47 @@ function checkInstances() {
  * EC2
  * ****************************************************************************/
 
+function checkEc2Instances(orders) {
+    return new Promise(function (resolve) {
 
+        listConfigurationEc2().then(function (ec2) {
+            ec2 = JSON.parse(ec2.body).data;
+            for (var i = 0; i < ec2.length; i++) {
+                var currentDesiredState = desiredState(ec2[i]);
+
+                if (currentDesiredState !== ec2[i].Status && currentDesiredState === "STOPPED" && ec2[i].Status === "RUNNING") {
+                    orders.push({"scope": "ec2", "instanceId": ec2[i].instanceId, "order": "stop"});
+                    stopEc2Instance(ec2[i].instanceId);
+                } else if (currentDesiredState !== ec2[i].Status && currentDesiredState === "RUNNING" && ec2[i].Status === "STOPPED") {
+                    orders.push({"scope": "ec2", "instanceId": ec2[i].instanceId, "order": "start"});
+                    startEc2Instance(ec2[i].instanceId);
+                }
+            }
+            return resolve(orders);
+        });
+
+    });
+}
+
+async function startEc2Instance(instanceId) {
+    if(dryRun) return true;
+    var params = {
+        InstanceIds: [instanceId]
+    };
+    await ec2.startInstances(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+    });
+}
+
+async function stopEc2Instance(instanceId) {
+    if(dryRun) return true;
+    var params = {
+        InstanceIds: [instanceId]
+    };
+    await ec2.stopInstances(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+    });
+}
 
 function listExistingEc2() {
     return new Promise(function (resolve) {
@@ -199,6 +226,16 @@ function listConfigurationEc2() {
 }
 
 function updateConfigurationEc2(values, params) {
+
+    for(let i=0;i<params.days.length;i++)
+    {
+        if(params.days[i].start === "" || params.days[i].end === "")
+        {
+            params.days[i].start = null;
+            params.days[i].end = null;
+        }
+    }
+
     let found = false;
     for (let n = 0; n < values.ec2.length; n++) {
         if (values.ec2[n].instanceId === params.instanceId) {
@@ -280,6 +317,47 @@ function describeEc2instance(values) {
  * RDS
  * ****************************************************************************/
 
+function checkRdsInstances(orders) {
+    return new Promise(function (resolve) {
+
+        listConfigurationRds().then(function (rds) {
+            rds = JSON.parse(rds.body).data;
+            for (var i = 0; i < rds.length; i++) {
+                var currentDesiredState = desiredState(rds[i]);
+                if (currentDesiredState !== rds[i].Status && currentDesiredState === "STOPPED" && rds[i].Status === "AVAILABLE") {
+                    orders.push({"scope": "rds", "instanceId": rds[i].instanceId, "order": "stop"});
+                    stopRdsInstance(rds[i].instanceId);
+                } else if (currentDesiredState !== rds[i].Status && currentDesiredState === "RUNNING" && rds[i].Status === "STOPPED") {
+                    orders.push({"scope": "rds", "instanceId": rds[i].instanceId, "order": "start"});
+                    startRdsInstance(rds[i].instanceId);
+                }
+            }
+            return resolve(orders);
+        });
+
+    });
+}
+
+async function startRdsInstance(instanceId) {
+    if(dryRun) return true;
+    var params = {
+        DBInstanceIdentifier: instanceId
+    };
+    await rds.startDBInstance(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+    });
+}
+
+async function stopRdsInstance(instanceId) {
+    if(dryRun) return true;
+    var params = {
+        DBInstanceIdentifier: instanceId
+    };
+    await rds.stopDBInstance(params, function (err, data) {
+        if (err) console.log(err, err.stack);
+    });
+}
+
 function listExistingRds() {
     return new Promise(function (resolve) {
         rds.describeDBInstances({}, function (err, data) {
@@ -288,7 +366,7 @@ function listExistingRds() {
                 var instances = [];
 
                 for (var i in data.DBInstances) {
-                    var instanceid = data.DBInstances[i].DBInstanceArn;
+                    var instanceid = data.DBInstances[i].DBInstanceIdentifier;
                     var name = data.DBInstances[i].DBInstanceIdentifier;
                     instances.push({instanceId: instanceid, Name: name});
                 }
@@ -304,9 +382,9 @@ function describeRdsinstance(values) {
     for (var i = 0; i < values.rds.length; i++) {
         instanceIds.push(values.rds[i].instanceId);
     }
-    if (instanceIds.length == 0) return values;
+    if (instanceIds.length === 0) return values;
 
-    const promise = new Promise(function (resolve) {
+    return new Promise(function (resolve) {
         var params = {
             Filters: [{
                 Name: 'db-instance-id',
@@ -321,7 +399,7 @@ function describeRdsinstance(values) {
 
                 var instances = [];
                 for (var i in data.DBInstances) {
-                    var instanceid = data.DBInstances[i].DBInstanceArn;
+                    var instanceid = data.DBInstances[i].DBInstanceIdentifier;
                     var name = data.DBInstances[i].DBInstanceIdentifier;
                     var state = data.DBInstances[i].DBInstanceStatus.toUpperCase();
                     instances.push({instanceId: instanceid, State: state, Name: name});
@@ -342,7 +420,6 @@ function describeRdsinstance(values) {
         });
 
     });
-    return promise;
 }
 
 function listConfigurationRds() {
@@ -360,6 +437,14 @@ function listConfigurationRds() {
 }
 
 function updateConfigurationRds(values, params) {
+    for(let i=0;i<params.days.length;i++)
+    {
+        if(params.days[i].start === "" || params.days[i].end === "")
+        {
+            params.days[i].start = null;
+            params.days[i].end = null;
+        }
+    }
     var found = false;
     for (var n = 0; n < values.rds.length; n++) {
         if (values.rds[n].instanceId === params.instanceId) {
@@ -419,9 +504,20 @@ function putConfigurationRds(params) {
  * COMMON
  * ****************************************************************************/
 
-function currentState(item) {
+function desiredState(item) {
+    var desiredFromPlanning = desiredStateFromPlanning(item);
+    var desiredFromException = desiredStateFromException(item);
+    if(desiredFromException !== desiredFromPlanning && desiredFromException !== "NONE")
+    {
+        return desiredFromException;
+    }
+    return desiredFromPlanning;
+}
+
+function desiredStateFromPlanning(item) {
     var d = (new Date());
     var weekDay = d.getDay();
+
     var dayConf = item.days[weekDay];
     if (dayConf.start === null || dayConf.end === null) return "STOPPED";
     let startHour = dayConf.start.substr(0, 2);
@@ -429,13 +525,44 @@ function currentState(item) {
     let endHour = dayConf.end.substr(0, 2);
     let endMinuts = dayConf.end.substr(3, 2);
 
-    let startIndex = Number(startHour + startMinuts);
-    let endIndex = Number(endHour + endMinuts);
-    let currentIndex = Number(d.getHours().toFixed(2) + d.getMinutes().toFixed(2));
+    let startIndex = (Number(startHour) * 60) + Number(startMinuts);
+    let endIndex = (Number(endHour) * 60) + Number(endMinuts);
+    let currentIndex = (Number(d.getHours()) * 60) + Number(d.getMinutes());
 
-    if (currentIndex < startIndex || currentIndex > endIndex) return "STOPPED";
+    if (currentIndex < startIndex || currentIndex >= endIndex) return "STOPPED";
 
     return "RUNNING";
+}
+
+function desiredStateFromException(item) {
+    if(item.exception !== null)
+    {
+        if(item.exception.startDate  === null || item.exception.endDate  === null || item.exception.startTime  === null || item.exception.endTime  === null)
+        {
+            return "NONE";
+        }
+        var d = (new Date());
+
+        let startIndex = Number(item.exception.startDate.replace("-",""));
+        let endIndex = Number(item.exception.endDate.replace("-",""));
+        let currentIndex = Number(d.toISOString().split('T')[0].replace("-",""));
+
+        if (currentIndex < startIndex || currentIndex >= endIndex) return "NONE";
+
+        let startHour = item.exception.startTime.substr(0, 2);
+        let startMinuts = item.exception.startTime.substr(3, 2);
+        let endHour = item.exception.endTime.substr(0, 2);
+        let endMinuts = item.exception.endTime.substr(3, 2);
+
+        let startTimeIndex = (Number(startHour) * 60) + Number(startMinuts);
+        let endTimeIndex = (Number(endHour) * 60) + Number(endMinuts);
+        let currentTimeIndex = (Number(d.getHours()) * 60) + Number(d.getMinutes());
+
+        if (currentTimeIndex < startTimeIndex || currentTimeIndex >= endTimeIndex) return "NONE";
+
+        return (item.exception.state === 1) ? "RUNNING": "STOPPED";
+    }
+    return "NONE";
 }
 
 function loadConfigurationFromFile() {
